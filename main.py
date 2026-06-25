6a29f8242763c45f40f275ab076531a75a0acdcd
Faizan <faizan@example.com>
2026-06-25 13:21:35 +0000

    setup 1

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from multi-agent-system!")
+import operator
+import os
+from typing import Annotated, TypedDict
+
+import psycopg
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, AnyMessage, HumanMessage, SystemMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.graph import END, START, StateGraph
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+print("Hello Agent!")
